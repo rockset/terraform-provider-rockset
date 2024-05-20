f3536e95070586ebd983b9b59c694dbe65ec2274
Martin Englund <pme@rockset.com>
2024-05-20 13:02:14 -0700

    fix tests after api change (#112)

--- testdata/scheduled_lambda_basic.tf
+++ testdata/scheduled_lambda_basic.tf
@@ -1,14 +1,18 @@
 resource rockset_query_lambda "test_query_lambda" {
   workspace = "acc"
-  name      = "{{ .QueryLambdaName}}"
+  name      = "{{ .QueryLambdaName }}"
   sql {
     query = "select 1"
   }
 }
 
+resource "rockset_api_key" "test" {
+  name = "{{ .QueryLambdaName }}"
+}
+
 resource "rockset_scheduled_lambda" "test_scheduled_lambda" {
   workspace = "acc"
-  apikey = "var.ROCKSET_APIKEY"
+  apikey = rockset_api_key.test.key
   cron_string = "{{ .CronString }}"
   query_lambda_name = rockset_query_lambda.test_query_lambda.name
   tag = "latest"
